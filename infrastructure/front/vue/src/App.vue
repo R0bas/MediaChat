b5fe941382d5ce8e84a71120ec9c3c81c572d823
R0bas <couscouskitchen99@gmail.com>
2025-02-08 13:33:41 +0100

    CSS Advanced

--- infrastructure/front/vue/src/App.vue
+++ infrastructure/front/vue/src/App.vue
@@ -24,17 +24,19 @@ const MediaIsImage = computed(() => {
 </script>
 
 <template>
-  <div class="flex flex-col items-start select-none bg-opacity-0" v-if="state.currentMediaChat">
+  <div class="flex flex-col items-start justify-between select-none bg-opacity-1 h-screen" v-if="state.currentMediaChat">
     <div id="avatar" class="flex flex-col justify-center items-center floating pt-4 pl-2">
-      <img class="rounded-3xl" :src="state.currentMediaChat.author.image" alt="avatar" width="50" height="50" />
-      <h2 class="text-center text-xl font-black uppercase">{{ state.currentMediaChat.author.name }}</h2>
+      <img class="rounded-full" :src="state.currentMediaChat.author.image" alt="avatar" width="75" height="75" />
+      <h2 class="text-center text-xl font-bold uppercase text-outline-black text-white">{{ state.currentMediaChat.author.name }}</h2>
     </div>
-    <video v-if="MediaIsVideo" @canplay="playVideo" ref="player" class="w-full" autoplay>
-      <source :src="state.currentMediaChat.media.url" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-    <img v-if="MediaIsImage" :src="state.currentMediaChat.media.url" alt="mediachat" class="w-full px-10"/>
-    <p class="w-full text-center text-3xl">{{ state.currentMediaChat.message }} </p>
+    <div class="w-full">
+      <video v-if="MediaIsVideo" @canplay="playVideo" ref="player" class="m-auto h-fit" autoplay>
+        <source :src="state.currentMediaChat.media.url" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <img v-if="MediaIsImage" :src="state.currentMediaChat.media.url" alt="mediachat" class="w-fit mx-10"/>
+    <p class="w-full text-center text-3xl uppercase text-outline-black text-white font-bold">{{ state.currentMediaChat.message }} </p>
   </div>
 
 </template>
@@ -55,4 +57,10 @@ const MediaIsImage = computed(() => {
             transform: translate(0, 0);
           }
         }
+
+        @layer utilities {
+    .text-outline-black {
+      text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+    }
+  }
     </style>
