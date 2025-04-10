6c3013820f3c25487978ea0107b3982fb0686e41
elkofy <nassimdaikh71@gmail.com>
2025-04-10 15:27:04 +0200

    session url

--- infrastructure/express/routes/routes.ts
+++ infrastructure/express/routes/routes.ts
@@ -3,11 +3,13 @@ import { GetUsersHandler } from "../controllers/GetUsersController";
 import { CobaltHandler } from "../controllers/CobaltController";
 import { TunnelHandler } from "../controllers/TunnelController";
 import express from "express";
+import { YtTokenHandler } from "../controllers/YtTokenController";
 
 const router = express.Router();
 router.post("/mediachats", createMediaChatHandler);
 router.get("/users", GetUsersHandler);
 router.post("/cobalt", CobaltHandler);
 router.get("/tunnel", express.raw(), TunnelHandler);
+router.get("/token", YtTokenHandler)
 
 export default router;
